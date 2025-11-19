214b432420d79e63fb3ea3f143ff607e10c2bd15
Piotr Czarnocki <236667541+piotr2235@users.noreply.github.com>
2025-11-20 00:35:24 +0100

    Update vite.config.ts

--- oszust/vite.config.ts
+++ oszust/vite.config.ts
@@ -1,7 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/oszust/', // <--- TO JEST NAJWAŻNIEJSZE
+})
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
